e7b078aa5a02aa7da903ba2bbe30b625c59a508f
Divine <105461777+Didii7@users.noreply.github.com>
2024-01-15 23:21:19 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+document.addEventListener('DOMContentLoaded', function () {
+	const tabButtons = document.querySelectorAll('.tab-button');
+	const imageContainers = document.querySelectorAll('.image-container .image');
+	const nav = document.querySelector('.nav');
+	const hamburger = document.querySelector('.hamburger');
+	const menu = document.querySelector('.menu');
+
+    function selectLink(link) {
+        const allLinks = document.querySelectorAll('nav a');
+        allLinks.forEach((item) => {
+            item.classList.remove('selected');
+        });
+    
+        
+        link.classList.add('selected');
+    }
+
+	window.addEventListener('scroll', function () {
+		if (this.window.scrollY > 80) {
+			nav.style.background = `rgba(0,0,0,0.5)`;
+		} else {
+			nav.style.background = 'transparent';
+		}
+	});
+	hamburger.addEventListener('click', function () {
+		menu.classList.toggle('show-mobile-menu');
+	});
+
+	tabButtons.forEach((button) => {
+		button.addEventListener('click', function () {
+			tabButtons.forEach((btn) => {
+				btn.classList.remove('active');
+			});
+			imageContainers.forEach((container) => {
+				container.style.display = 'none';
+			});
+
+			const category = button.getAttribute('data-category');
+
+			button.classList.add('active');
+
+			document.querySelector(`.image.${category}`).style.display = 'grid';
+		});
+	});
+	document.querySelector(".tab-button[data-category='events']").click();
+});
